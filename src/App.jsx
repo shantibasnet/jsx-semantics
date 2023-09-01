8ef777ac738abe0b2842f40c527546dc04b38197
shanti basnet <shanti.basnet@mnsu.edu>
2023-08-31 20:24:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+export default function App() {
+  return (
+    <div>
+      <header>
+        <h1>Healthy Habits</h1>
+      </header>
+      <main>
+        <div class="center-box">
+          <ol>
+            <li>Wake up</li>
+            <li>EAT</li>
+            <li>Exercise</li>
+            <li>SLeep</li>
+          </ol>
+          <div class="box"></div>
+        </div>
+      </main>
+      <footer>
+        <p>Paint and Live</p>
+        <aside>
+          <a href="https://health.clevelandclinic.org/11-simple-health-habits-worth-adopting-into-your-life/">
+            Stay healthy
+          </a>
+        </aside>
+      </footer>
+    </div>
+  );
+}
